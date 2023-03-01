fc641165a8648b7187f9bec70231417c2e0ca268
Faizal_Husain_Adiasha <faizalhusainadiasha@students.undip.ac.id>
2023-03-01 16:53:50 +0700

    update titik class

--- Praktikum 1/Titik.java
+++ Praktikum 1/Titik.java
@@ -4,9 +4,9 @@
 // Tanggal : 22 Februari 2023
 // Deskripsi : Class Titik
 class Titik{
-    double absis;
-    double ordinat;
-    static double counterTitik;
+    private double absis;
+    private double ordinat;
+    private static int counterTitik;
 
     Titik() {
         absis = 0;
@@ -18,21 +18,21 @@ class Titik{
         ordinat = o;
         counterTitik++;
     }
-    void setAbsis(double a) {
+    public void setAbsis(double a) {
         absis = a ;
     }
 
-    void setOrdinat(double o) {
+    public void setOrdinat(double o) {
         ordinat = o;
     }
-    double getAbsis() {
+    public double getAbsis() {
         return absis;
     }
 
-    double getOrdinat() {
+    public double getOrdinat() {
         return ordinat;
     }
-    double getCounterTitik() {
+    public double getCounterTitik() {
         return counterTitik;
     }
     
