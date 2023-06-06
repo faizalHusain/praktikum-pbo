a10d282a5488a6e729c6f95a34166b3fb34c1b7c
Faizal_Husain_Adiasha <adihusain0@gmail.com>
2023-06-06 21:37:34 +0700

    add praktikum 10

---
+++ praktikum 10/LambdaHashmap.java
@@ -0,0 +1,15 @@
+// LambdaHashmap.java 6/6/23
+// Penulis  : Faizal Husain Adiasha
+// NIM      : 24060121140115
+// Deskripsi: Kelas Lambda HashMap
+import java.util.HashMap;
+public class LambdaHashmap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("24060121140115", "Faizal Husain Adiasha");
+        map.put("24060121140063", "Givandra Haikal Adjie");
+        map.put("24060121140064", "Hafizh Fadhlurrahman");
+
+        map.forEach((nim, nama) -> System.out.println("NIM :"+ nim + " Nama :"+ nama));
+    }
+}
